2266c7ca48e9cda75fb079d8b55ca5a645538a63
neilmallory <neilmallory@neils-macbook-pro.local>
2017-05-20 19:53:45 +0100

    Lecture 75 complete Table view implemented

--- rainyShineyCloudy/WeatherViewController.swift
+++ rainyShineyCloudy/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -19,9 +19,26 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        forcastTable.delegate = self
+        forcastTable.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
 }
 
